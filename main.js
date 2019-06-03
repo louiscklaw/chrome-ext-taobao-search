ae9a14e223db4390f9f19195a7591437c9ae6151
logickee <logickee@gmail.com>
2019-06-03 19:49:34 +0800

    reorder search,

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 searchItem = function(word){
   var query = word.selectionText;
 
+  var tmallSearchParam = '&type=p&vmarket=&spm=875.7931836%2FB.a2227oh.d100';
+  var tmallTargetUrl = "https://list.tmall.com/search_product.htm?q=" + query+ tmallSearchParam;
+  console.log(tmallTargetUrl);
+  chrome.tabs.create({url: tmallTargetUrl});
+
   var taobaoSearchParam = '&sort=sale-desc';
   var taobaoTargetUrl = "https://s.taobao.com/search?q=" + query+ taobaoSearchParam;
   console.log(taobaoTargetUrl);
   chrome.tabs.create({url: taobaoTargetUrl});
 
-  var tmallSearchParam = '&type=p&vmarket=&spm=875.7931836%2FB.a2227oh.d100';
-  var tmallTargetUrl = "https://list.tmall.com/search_product.htm?q=" + query+ tmallSearchParam;
-  console.log(tmallTargetUrl);
-  chrome.tabs.create({url: tmallTargetUrl});
 };
 
 chrome.contextMenus.create({
