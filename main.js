c30f8ac2301e70ff3b5d708814760b3deffc97b2
logickee <logickee@gmail.com>
2019-06-07 18:19:17 +0800

    update for szlcsc,

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 searchItem = function(word){
   var query = word.selectionText;
 
+  // https://so.szlcsc.com/global.html?k=esp8266ex
+  // var tmallSearchParam = '&type=p&vmarket=&spm=875.7931836%2FB.a2227oh.d100';
+  var tmallTargetUrl = "https://so.szlcsc.com/global.html?k=" + query;
+  console.log(tmallTargetUrl);
+  chrome.tabs.create({url: tmallTargetUrl});
+
   var tmallSearchParam = '&type=p&vmarket=&spm=875.7931836%2FB.a2227oh.d100';
   var tmallTargetUrl = "https://list.tmall.com/search_product.htm?q=" + query+ tmallSearchParam;
   console.log(tmallTargetUrl);
