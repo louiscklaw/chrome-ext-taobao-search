c47d1a2b67a0b32945f02ff085aaf93700a6bcf3
logickee <logickee@gmail.com>
2019-07-04 15:01:43 +0800

    update,

--- main.js
+++ main.js
@@ -3,24 +3,14 @@ searchItem = function(word){
 
   // https://so.szlcsc.com/global.html?k=esp8266ex
   // var tmallSearchParam = '&type=p&vmarket=&spm=875.7931836%2FB.a2227oh.d100';
-  var tmallTargetUrl = "https://so.szlcsc.com/global.html?k=" + query;
+  var tmallTargetUrl = "http://www.aastocks.com/tc/stocks/quote/quick-quote.aspx?symbol=" + query;
   console.log(tmallTargetUrl);
   chrome.tabs.create({url: tmallTargetUrl});
-
-  var tmallSearchParam = '&type=p&vmarket=&spm=875.7931836%2FB.a2227oh.d100';
-  var tmallTargetUrl = "https://list.tmall.com/search_product.htm?q=" + query+ tmallSearchParam;
-  console.log(tmallTargetUrl);
-  chrome.tabs.create({url: tmallTargetUrl});
-
-  var taobaoSearchParam = '&sort=sale-desc';
-  var taobaoTargetUrl = "https://s.taobao.com/search?q=" + query+ taobaoSearchParam;
-  console.log(taobaoTargetUrl);
-  chrome.tabs.create({url: taobaoTargetUrl});
 
 };
 
 chrome.contextMenus.create({
-  title: "Search in Taobao",
+  title: "Search in aastock",
   contexts:["selection"],
   onclick: searchItem
 });
